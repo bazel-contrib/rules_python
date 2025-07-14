a3ee5c1053ebc08354014d9d136ae2dc1c280a4d
Zhongpeng Lin <zplin@uber.com>
2025-07-14 08:14:57 -0700

    addressing comments

--- gazelle/python/python_test.go
+++ gazelle/python/python_test.go
@@ -141,6 +141,7 @@ func testPath(t *testing.T, gazellePath, name string, files []bazel.RunfileEntry
 				if err != nil {
 					return err
 				}
+				t.Logf("%q exists", strings.TrimPrefix(path, testdataDir))
 				return nil
 			})
 		})
