9dd58d791d49f538fec0f5865aecab75164b4cad
Jonathan Woodbury <jpwoodbu@gmail.com>
2025-08-02 20:15:38 -0400

    Improve error messages
    This commit also makes an attempt to minimize the amount of code within the `try` block for importing `realine`.

--- python/bin/repl_stub.py
+++ python/bin/repl_stub.py
@@ -17,7 +17,27 @@ The logic for PYTHONSTARTUP is handled in python/private/repl_template.py.
 console_locals = globals().copy()
 
 import code
+import rlcompleter
 import sys
+
+
+class DynamicCompleter(rlcompleter.Completer):
+    """
+    A custom completer that dynamically updates its namespace to include new
+    imports made within the interactive session.
+    """
+
+    def __init__(self, namespace):
+        # Store a reference to the namespace, not a copy, so that changes to the namespace are
+        # reflected.
+        self.namespace = namespace
+
+    def complete(self, text, state):
+        # Update the completer's internal namespace with the current interactive session's locals
+        # and globals.  This is the key to making new imports discoverable.
+        rlcompleter.Completer.__init__(self, self.namespace)
+        return super().complete(text, state)
+
 
 if sys.stdin.isatty():
     # Use the default options.
@@ -31,23 +51,6 @@ else:
 # Set up tab completion.
 try:
     import readline
-    import rlcompleter
-
-    class DynamicCompleter(rlcompleter.Completer):
-        """
-        A custom completer that dynamically updates its namespace to include new
-        imports made within the interactive session.
-        """
-        def __init__(self, namespace):
-            # Store a reference to the namespace, not a copy, so that changes to the namespace are
-            # reflected.
-            self.namespace = namespace
-
-        def complete(self, text, state):
-            # Update the completer's internal namespace with the current interactive session's locals
-            # and globals.  This is the key to making new imports discoverable.
-            rlcompleter.Completer.__init__(self, self.namespace)
-            return super().complete(text, state)
 
     completer = DynamicCompleter(console_locals)
     readline.set_completer(completer.complete)
@@ -59,9 +62,15 @@ try:
     elif "GNU readline" in readline.__doc__:  # type: ignore
         readline.parse_and_bind("tab: complete")
     else:
-        print("Could not enable tab completion!")
+        print(
+            "Could not enable tab completion: "
+            "unable to determine readline backend"
+        )
 except ImportError:
-    print("Could not enable tab completion!")
+    print(
+        "Could not enable tab completion: "
+        "readline module not available on this platform"
+    )
 
 # We set the banner to an empty string because the repl_template.py file already prints the banner.
 code.interact(local=console_locals, banner="", exitmsg=exitmsg)
