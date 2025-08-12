70509e1b3c67b4e9114e7cbc5168d8e528dfd8b5
Laramie Leavitt <lar@google.com>
2025-08-11 23:23:41 -0700

    Complete extension rename: echo -> echo_ext

--- tests/integration/local_toolchains/echo_test.py
+++ tests/integration/local_toolchains/echo_test.py
@@ -8,4 +8,4 @@ import unittest
 class ExtensionTest(unittest.TestCase):
 
     def test_echo_extension(self):
-        self.assertEqual(echo.echo(42, "str"), tuple(42, "str"))
+        self.assertEqual(echo_ext.echo(42, "str"), tuple(42, "str"))
