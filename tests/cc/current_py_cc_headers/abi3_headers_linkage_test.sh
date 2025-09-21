aa8b96f3a23822b2d429f9435cfa206feb0308fb
Richard Levasseur <rlevasseur@google.com>
2025-09-20 20:01:25 -0700

    try basic test to check linkage

---
+++ tests/cc/current_py_cc_headers/abi3_headers_linkage_test.sh
@@ -0,0 +1,22 @@
+
+echo "hi =========="
+echo "hi =========="
+
+# --- begin runfiles.bash initialization v3 ---
+# Copy-pasted from the Bazel Bash runfiles library v3.
+set -uo pipefail; set +e; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v3 ---
+set +e
+
+echo $RUNFILES_DIR
+lib=$(rlocation _main/tests/cc/current_py_cc_headers/libbin_abi3.so)
+
+dumpbin /nologo /DEPENDENTS "$lib"
+
+exit 1
