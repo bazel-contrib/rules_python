473f801f8f7fa72241d49591e4b405b81dec30a1
Richard Levasseur <rlevasseur@google.com>
2026-04-29 09:48:28 -0700

    fix syntax error

--- tests/venv_site_packages_libs/whl_scripts_runnable_test.py
+++ tests/venv_site_packages_libs/whl_scripts_runnable_test.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 BAZEL_8_OR_LATER = bool(int(os.environ.get("BAZEL_8_OR_LATER", "0")))
 
+
 class WhlScriptsRunnableTest(unittest.TestCase):
     maxDiff = None
 
@@ -65,9 +66,8 @@ class WhlScriptsRunnableTest(unittest.TestCase):
     # This should really check for 8.5 instead of 8+, but we test with 8.6
     # so it's close enough for our purposes.
     @unittest.skipUnless(
-            BAZEL_8_OR_LATER,
-            "bazel 8.5 and lower uses wheel.py, which rewrites #!pythonw to
-            #!python",
+        BAZEL_8_OR_LATER,
+        "bazel 8.5 and lower uses wheel.py, which rewrites #!pythonw to #!python",
     )
     def test_pythonw_script(self):
         script_path = self._get_script_path("whl_with_data1_pythonw")
