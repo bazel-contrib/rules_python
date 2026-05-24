58683f6e4e50b989ffb8a22b85e8a5a2e95e921c
Ignas Anikevicius <240938+aignas@users.noreply.github.com>
2026-05-24 17:18:25 +0900

    also support 3.8

--- examples/bzlmod/test.py
+++ examples/bzlmod/test.py
@@ -58,7 +58,7 @@ class ExampleTest(unittest.TestCase):
                 f"sys.path has {len(sys.path)} items:\n    {all_paths}",
             )
 
-        first_item, last_item = sys.path[0], sys.path[-1]
+        first_item, _ = sys.path[0], sys.path[-1]
         self.assertFalse(
             first_item.endswith("coverage"),
             f"Expected the first item in sys.path '{first_item}' to not be related to coverage",
