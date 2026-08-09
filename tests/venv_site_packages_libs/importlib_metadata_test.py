bd0da7ea8f6f47e7418ffa45cc91a5234bc62e46
Richard Levasseur <richardlev@gmail.com>
2026-08-09 03:57:14 +0000

    test(venv): sort expected paths in importlib_metadata_test
    Sort expected_paths list alphabetically in source for improved readability.

--- tests/venv_site_packages_libs/importlib_metadata_test.py
+++ tests/venv_site_packages_libs/importlib_metadata_test.py
@@ -11,27 +11,27 @@ class ImportlibMetadataTest(unittest.TestCase):
         )
 
         expected_paths = [
-            "whl_with_data1-1.0.data/platlib/whl_with_data1/platlib_file.txt",
-            "whl_with_data1-1.0.data/scripts/whl_with_data1_script",
-            "whl_with_data1-1.0.data/scripts/whl_script.sh",
-            "whl_with_data1-1.0.data/headers/whl_with_data1/header_file.h",
-            "whl_with_data1-1.0.data/purelib/whl_with_data1/data_file.txt",
-            "whl_with_data1-1.0.data/data/whl_with_data1/data_data_file.txt",
-            "whl_with_data1-1.0.data/data/whl_with_data1/data_data_file.txt",
+            "whl_with_data1-1.0.data/data/bin/data_overlap.sh",
+            "whl_with_data1-1.0.data/data/include/data_overlap.h",
             "whl_with_data1-1.0.data/data/overlap/both.txt",
             "whl_with_data1-1.0.data/data/overlap/data1.txt",
-            "whl_with_data1-1.0.data/scripts/overlap/both.sh",
-            "whl_with_data1-1.0.data/scripts/overlap/script1.sh",
+            "whl_with_data1-1.0.data/data/site-packages/data_overlap.py",
+            "whl_with_data1-1.0.data/data/whl_with_data1/data_data_file.txt",
+            "whl_with_data1-1.0.data/data/whl_with_data1/data_data_file.txt",
+            "whl_with_data1-1.0.data/headers/data_overlap.h",
             "whl_with_data1-1.0.data/headers/overlap/both.h",
             "whl_with_data1-1.0.data/headers/overlap/header1.h",
-            "whl_with_data1-1.0.data/scripts/data_overlap.sh",
-            "whl_with_data1-1.0.data/data/bin/data_overlap.sh",
-            "whl_with_data1-1.0.data/headers/data_overlap.h",
-            "whl_with_data1-1.0.data/data/include/data_overlap.h",
+            "whl_with_data1-1.0.data/headers/whl_with_data1/header_file.h",
+            "whl_with_data1-1.0.data/platlib/whl_with_data1/platlib_file.txt",
             "whl_with_data1-1.0.data/purelib/data_overlap.py",
-            "whl_with_data1-1.0.data/data/site-packages/data_overlap.py",
+            "whl_with_data1-1.0.data/purelib/whl_with_data1/data_file.txt",
+            "whl_with_data1-1.0.data/scripts/data_overlap.sh",
+            "whl_with_data1-1.0.data/scripts/overlap/both.sh",
+            "whl_with_data1-1.0.data/scripts/overlap/script1.sh",
+            "whl_with_data1-1.0.data/scripts/whl_script.sh",
+            "whl_with_data1-1.0.data/scripts/whl_with_data1_script",
         ]
-        file_paths = [str(f).replace("\\", "/") for f in files]
+        file_paths = sorted(str(f).replace("\\", "/") for f in files)
         self.assertEqual(file_paths, expected_paths)
 
         for f in files:
