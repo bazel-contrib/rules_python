3a6697f9a6ebe7a8a5088b54329462e5eb6cfc47
Ignas Anikevicius <240938+aignas@users.noreply.github.com>
2026-08-10 00:35:43 +0900

    wip uv pip compile directory support for windows

--- python/uv/private/template/uv_pip_compile.bat
+++ python/uv/private/template/uv_pip_compile.bat
@@ -1,8 +1,19 @@
 @echo off
-if not defined BUILD_WORKSPACE_DIRECTORY goto :else
+if not defined BUILD_WORKSPACE_DIRECTORY goto :build
+
 set "out=%BUILD_WORKSPACE_DIRECTORY%\{{src_out}}"
 "{{args}}" --output-file "%out%" %*
 exit /b %ERRORLEVEL%
 
-:else
-"{{args}}" %*
+:build
+set "out={{out}}"
+if exist "{{src_out}}" copy /Y "{{src_out}}" "%out%" >nul
+set "__srcs="$SRCS""
+if defined __srcs (
+    for %%s in (%__srcs%) do (
+        for %%d in ("{{rootdir}}\%%s") do mkdir "%%~dpd" >nul 2>&1
+        copy /Y "%%s" "{{rootdir}}\%%s" >nul
+    )
+)
+"{{args}}"
+exit /b %ERRORLEVEL%
